ce8112f45c00d9fdc803074341142d2e76f659e7
veenuj <veenuj3112@gmail.com>
2026-09-07 18:10:00 +0530

    Migrate from SQL Server to Neon PostgreSQL

--- Program.cs
+++ Program.cs
@@ -70,4 +70,12 @@ app.MapControllerRoute(
 
 app.MapHub<FactoryHub>("/factoryHub");
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
+
 app.Run();
