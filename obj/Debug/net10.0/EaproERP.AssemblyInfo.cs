c8e3c4e8c56fab283e7f8790603f28d63e11665c
veenuj <veenuj3112@gmail.com>
2026-09-07 19:29:33 +0530

    Showing Password

--- obj/Debug/net10.0/EaproERP.AssemblyInfo.cs
+++ obj/Debug/net10.0/EaproERP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EaproERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa05953bdeee8c74429e5d90b7fc1948614dd07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e77d1beafc91e92d1196d4fb421619479d39a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EaproERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EaproERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
